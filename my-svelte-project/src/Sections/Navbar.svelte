1a0e08418124018d28a12304a3a5b82657276f8e
tizah <davidzagi93@gmail.com>
2020-08-12 18:00:53 +0100

    Card app with svelte

---
+++ my-svelte-project/src/Sections/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title;
+</script>
+
+<style>
+  nav {
+    width: 100%;
+    text-align: center;
+    color: #fff;
+    background-color: #333;
+    padding: 0.5rem 0;
+    font-size: 2rem;
+  }
+</style>
+
+<nav>{title}</nav>
---
+++ my-svelte-project/src/UI/Form.svelte
@@ -0,0 +1,44 @@
+<script>
+  let disabled = true;
+  let name = "";
+  let firstInput = "";
+  let active = false;
+
+  function keyPressed(event) {
+    console.log(event);
+
+    event.key === "Shift" ? "" : (disabled = false);
+  }
+
+  function resetForm() {
+    firstInput.focus();
+    name = "";
+    disabled = true;
+  }
+</script>
+
+<style>
+  .active {
+    border-color: hsl(211, 63%, 35%);
+    box-shadow: 0 1px 3px hsla(211, 63%, 35%, 0.4);
+  }
+</style>
+
+<form>
+  <input
+    type="text"
+    placeholder="Please enter your name"
+    class:active
+    on:focus={() => {
+      active = true;
+    }}
+    on:blue={() => {
+      active = false;
+    }}
+    on:keydown={keyPressed}
+    bind:value={name}
+    bind:this={firstInput} />
+
+  <button {disabled} on:click={resetForm}>Reset</button>
+</form>
+{name}
